6415e41552e100d6d5be0f29442b0bca49942260
Chris Wang <bpploabc@gmail.com>
2021-09-14 15:47:54 +0800

    fix test script error

--- test/images.spec.js
+++ test/images.spec.js
@@ -1,38 +1,26 @@
 const { getImage, flipImage } = require('../images')
-const sharp = require('sharp')
 const fs = require('fs').promises
 
-jest.mock('sharp', () => {
-    const result = {
-        toBuffer: jest.fn().mockReturnThis(),
-        toFile: jest.fn().mockReturnThis(),
-        flip: jest.fn().mockReturnThis(),
-        flop: jest.fn().mockReturnThis(),
-    }
-    return jest.fn(() => result)
-})
+jest.setTimeout(15000)
 
 describe('test', () => {
 
     let local_image = null
-    let local_flipped_image = sharp('./flipped_image.jpeg').toBuffer()
+    let local_flipped_image = fs.readFile('./test/flipped_image.jpeg')
     let image = null
     let flipped_image = null
 
     it('test getImage', async () => {
         image = await getImage('https://images.unsplash.com/photo-1631086459990-06bc4d7ad6cf')
         local_image = await fs.readFile('./test/image.jpeg')
-        await expect(image.equals(local_image)).toBe(true)
+        await expect(image).toEqual(local_image)
     })
 
     it('test flipImage', async () => {
         flipped_image = await flipImage(image)
-        await expect(flipped_image).toEqual(local_flipped_image)
+        await fs.writeFile('./test/result.jpeg', flipped_image)
+        const result = await fs.readFile('./test/result.jpeg')
+        await expect(result).toEqual(await local_flipped_image)
     })
 
-    it('test final result', async () => {
-        await sharp(flipped_image).toFile('./image/flipped_image.jpeg')
-        let result = await sharp('./image/flipped_image.jpeg').toBuffer()
-        await expect(result).toEqual(local_flipped_image)
-    })
 })
